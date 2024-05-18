050e7d00394b1e908a32fa7b14d8d847953c29aa
Tubagus <54710482+0xtbug@users.noreply.github.com>
2024-05-18 07:18:46 +0700

    (refactor) remove title

--- functions/functions.php
+++ functions/functions.php
@@ -17,10 +17,10 @@ function loginUser($email, $password, $conn) {
             $_SESSION['user'] = $email;
             alert_timer("Login berhasil.", "../pages/dashboard.php");
         } else {
-            alert("error", "Login Gagal", "Kata sandi salah.", "");
+            alert("error", "password salah.", "");
         }
     } else {
-        alert("error", "Login Gagal", "Email tidak terdaftar.", "");
+        alert("error", "Email tidak terdaftar.", "");
     }
 }
 
@@ -29,9 +29,9 @@ function registerUser($email, $password, $confirm_password, $conn) {
     $password = $conn->real_escape_string($password);
     $confirm_password = $conn->real_escape_string($confirm_password);
 
-    // Cek jika kata sandi dan konfirmasi kata sandi cocok
+    // Cek jika password dan konfirmasi password cocok
     if ($password != $confirm_password) {
-        alert("error", "Register Gagal", "Kata sandi dan konfirmasi kata sandi tidak cocok.", "register.php");
+        alert("error", "password dan konfirmasi password tidak cocok.", "register.php");
         exit;
     }
 
@@ -40,21 +40,21 @@ function registerUser($email, $password, $confirm_password, $conn) {
     if ($query->num_rows > 0) {
         $user = $query->fetch_assoc();
         if ($user['status'] == 'off') {
-            // Jika status off, update kata sandi dan ubah status ke on
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT); // Mengenkripsi kata sandi baru
+            // Jika status off, update password dan ubah status ke on
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT); // Mengenkripsi password baru
             $update = $conn->query("UPDATE users SET password='$hashed_password', status='on' WHERE email='$email'");
 
             if ($update) {
                 alert_timer("Pendaftaran berhasil, silakan login.", "login.php");
             } else {
-                alert("error", "Register Gagal", "Terjadi kesalahan.", "");
+                alert("error", "Terjadi kesalahan.", "");
             }
         } elseif ($user['status'] == 'on') {
             // Jika status sudah on, beri tahu bahwa akun tidak dapat dibuat
-            alert("error", "Register Gagal", "Email Anda sudah terdaftar di database.", "");
+            alert("error", "Email Anda sudah terdaftar di database.", "");
         }
     } else {
-        alert("error", "Register Gagal", "Email Anda belum terdaftar di database, akun tidak dapat dibuat.", "");
+        alert("error", "Email Anda belum terdaftar di database, akun tidak dapat dibuat.", "");
     }
 }
 ?>
