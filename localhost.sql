36a1ce307e9e08432221d05b0ce01599bd44b786
0xtbug <tubagus.nabil19@gmail.com>
2024-05-20 12:52:17 +0700

    update database

--- localhost.sql
+++ localhost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 17, 2024 at 11:54 PM
+-- Generation Time: May 20, 2024 at 05:51 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -26,10 +26,10 @@ USE `gubook`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `mahasiswa`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `mahasiswa` (
   `id` int NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -37,32 +37,87 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `status`) VALUES
-(1, '2210631170110@student.unsika.ac.id', '$2y$10$oxfW3M1VYPFlOEMBhHlX.e7uWCMh6HKs49TjlNIH2vG3tAl2nd2ci', 'on');
+INSERT INTO `mahasiswa` (`id`, `email`, `password`, `status`) VALUES
+(1, '2210631170110@student.unsika.ac.id', '$2y$10$sNatiF97BFmchi6.ghmMtOI2sA0SKWCg15jS9/fE03UiRpvHbjCS6', 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengurus`
+--
+
+CREATE TABLE `pengurus` (
+  `id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL,
+  `nama` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `nama`) VALUES
+(1, 'mahasiswa'),
+(2, 'pengurus');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `mahasiswa`
 --
-ALTER TABLE `users`
+ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `pengurus`
+--
+ALTER TABLE `pengurus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `mahasiswa`
 --
-ALTER TABLE `users`
+ALTER TABLE `mahasiswa`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pengurus`
+--
+ALTER TABLE `pengurus`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
